2fc8e62cf92f55ce2853a54ae4d632969d0ee5b1
phucthinh2609 <phucthinh2609@gmail.com>
2022-04-23 08:13:31 +0700

    update 23.4

---
+++ src/Bai6_Extands/Shape.java
@@ -0,0 +1,46 @@
+package Bai4_Class_Object;
+
+public class Shape {
+    private String color = "green";
+    private boolean filled = true;
+
+    public Shape(){
+    }
+
+    public Shape(String color, boolean filled){
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+
+    public boolean isFilled(){
+        return filled;
+    }
+
+    public String toString(){
+        return "A Shape with color of "
+                + getColor()+" and "
+                + (isFilled()?"filled":"not filled");
+    }
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("blue", false);
+        System.out.println(shape);
+    }
+}
+
+
