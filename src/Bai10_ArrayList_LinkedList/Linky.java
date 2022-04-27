02eb83846980ef3a8cbef538ddb16fc7599388b9
phucthinh2609 <phucthinh2609@gmail.com>
2022-04-27 10:58:00 +0700

    Mylist

---
+++ src/Bai10_ArrayList_LinkedList/Linky.java
@@ -0,0 +1,7 @@
+package Bai10_ArrayList_LinkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        LinkedList linkedList = new java.util.LinkedList<>()
+    }
+}
---
+++ src/Bai10_ArrayList_LinkedList/myList/MyList.java
@@ -0,0 +1,50 @@
+package Bai10_ArrayList_LinkedList.myList;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+
+    public MyList(){
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void ensureCap(){
+        int newSize = elements.length*2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(E e){
+        if(size == elements.length){
+            ensureCap();
+        }
+        elements[size++]= e;
+    }
+
+    public E get(int index){
+        if(index<0 || index>=size){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + index );
+        }
+        return (E) elements[index];
+    }
+
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>();
+
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+
+        System.out.println("Element 2: "+myList.get(2));
+        System.out.println("Element 1: "+myList.get(1));
+
+//        myList.get(5);
+//        System.out.println("Element 5: "+ myList.get(5));
+
+        myList.get(-1);
+        System.out.println("Element -1: "+ myList.get(-1));
+
+    }
+}
